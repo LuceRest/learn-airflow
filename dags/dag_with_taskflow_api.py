fad531569e4e3aa3f9a7fb705ab92771a93b2f43
AcilRestu12 <arieskarestu214@gmail.com>
2025-04-03 02:52:46 +0700

    P07 - Airflow TaskFlow API

---
+++ dags/dag_with_taskflow_api.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+
+default_args = {
+    'owner': 'acilrestu12',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+@dag(dag_id='dag_with_taskflow_api_v02', 
+    default_args=default_args, 
+    start_date=datetime(2025, 3, 31), 
+    schedule_interval='@daily')
+def hellow_world_etl():
+    
+    @task
+    def get_name():
+        return {
+            'first_name': 'Jerry',
+            'last_name': 'Fridman'
+        }
+    
+    @task
+    def get_age():
+        return 19
+    
+    @task
+    def greet(name_dict, age):
+        first_name = name_dict['first_name']
+        last_name = name_dict['last_name']
+        print(f"Hello world! My name is {first_name} {last_name}"
+            f"and I am {age} years old!")
+    # def greet(first_name, last_name, age):
+    #     print(f"Hello world! My name is {first_name} {last_name}"
+    #         f"and I am {age} years old!")
+        
+    name_dict = get_name()
+    age = get_age()
+    greet(name_dict, age)
+    # greet(first_name=name_dict['first_name'],
+    #       last_name=name_dict['last_name'],
+    #       age=age)
+    
+    
+greet_dag = hellow_world_etl()
